9500eec7214ce1169c7706bbe6d531527342cbb9
Schi8m <pavel1001004@gmail.com>
2021-03-02 12:39:31 +0300

    Ruby ready 0.1.1

---
+++ Ruby/Strings.rb
@@ -0,0 +1,6 @@
+name = "Павел"
+surname = "Евелин"
+puts (name + surname)
+nickname = "schi8m"
+age = 21
+puts ("Привет, меня зовут " + name.to_s + ", фамилия: " + surname.to_s + "\n. Мне " + age.to_s + " лет. \nМой никнейм: \"" + nickname.to_s + "\".")
--- Ruby/hello_world.rb
+++ Ruby/hello_world.rb
@@ -1,2 +1,3 @@
 #Hello_World
 puts 'Hello world'
+print "Hello world\n"
