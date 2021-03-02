36f7bd5c2a6d4d73cf8240b60477a6a926f2e760
Schi8m <pavel1001004@gmail.com>
2021-03-03 02:40:44 +0300

    Java 0.1.4

---
+++ Java/Sum.java
@@ -0,0 +1,18 @@
+package com.company;
+import java.util.Scanner;
+
+public class Main
+{
+	public static void main(String[] args) {
+		int a, b, c, do_not_close_the_window;
+		Scanner num = new Scanner(System.in);
+		System.out.println("Введите первое слагаемое:");
+		a = num.nextInt();
+		System.out.println("Введите второе слагаемое:");
+		b = num.nextInt();
+		c = a + b;
+		System.out.println(a + " + " + b + " = " + c);
+		do_not_close_the_window = num.nextInt();
+		
+	}
+}
