f66dce6dbcc1799baec5bd6b491cf61cca60d1d1
Schi8m <pavel1001004@gmail.com>
2021-03-09 18:16:04 +0300

    0.2.2

--- Input numbers until == 0/C/a.c
+++ Input numbers until == 0/C/a.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 int main(){
 	int a;
+	if (a!=0){
 	while (a!=0){
 		scanf("%d", &a);
+		}
 
 	}
 }
--- Input numbers until == 0/Ruby/a.rb
+++ Input numbers until == 0/Ruby/a.rb
@@ -1,5 +1,7 @@
 a=1
+if (a!=0) 
 until (a == 0)
 	a = gets
 	a = a.to_i
+end
 end
