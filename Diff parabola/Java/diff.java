7aa55a2c975c8999b632dd1b5f3d849b83dc5bf0
Schi8m <pavel1001004@gmail.com>
2021-03-28 22:47:19 +0300

    0.3.7

--- Diff parabola/Java/diff.java
+++ Diff parabola/Java/diff.java
@@ -2,26 +2,29 @@ import java.util.Scanner;
 
 public class diff{
 	public static void main(String[] args) {
-		float x1,x2,d,x,max,dx,dy1,dy2,delt;
-		Scanner num = new Scanner(System.in);
-		x1 = num.nextFloat();
-		x2 = num.nextFloat();
-		d=x2-x1;
-		dx=d/100;
-		x=x1;
-		max=(6*x1+6)*dx;
-		while (x<x2){
-			dy1=(6*x+6)*dx;
-			x=x+dx;
-			dy2=(6*x+6)*dx;
-			delt=dy2-dy1;
-			if (delt>max){
-				max=delt;
+		float x1, x2, dx, dy, a, b, max, i;
+		Scanner numScan = new Scanner(System.in);
+		a = numScan.nextFloat();
+		b = numScan.nextFloat();
+		dx = (b - a)/100;
+		x1 = a;
+		x2 = a + dx;
+		max = function(x1, x2);
+		for (i = a; i <= b; i += dx){
+			x1 = i;
+			x2 = x1 + dx;
+			dy = function(x1, x2);
+			if (dy > max){
+				max = dy;
 			}
-			
-		
 		}
+		
 		System.out.println(max);
 
+	}
+	public static float function (float funcX1, float funcX2){
+		float funcResult;
+		funcResult = (3 * funcX2*funcX2 - 893 * funcX2 - 2000) - (3 * funcX1*funcX1 - 893 * funcX1 - 2000);
+		return funcResult;
 	}
 }
