4c0d15a22e19d74bcd70106383821f8831c942ab
Schi8m <pavel1001004@gmail.com>
2021-03-28 23:01:30 +0300

    0.3.8

--- Arrays/Main diagonal/Java/mainDiagonal.java
+++ Arrays/Main diagonal/Java/mainDiagonal.java
@@ -2,22 +2,22 @@ import java.util.Scanner;
 import java.util.Arrays;
 public class mainDiagonal{
 	public static void main(String[] args){
-		int index, i,j,k;
+		int arrayLength, i,j,k;
 		j = 0;
 		k = 0;
-		index = (int)(Math.random()*10);
-		int[][] matrix = new int [index+1][index+1];
+		arrayLength = (int)(Math.random()*10);
+		int[][] matrix = new int [arrayLength+1][arrayLength+1];
 
 		System.out.println("Матрица:");
-		for (i=0;i<=index;i++){
-			for(j=0; j<=index; j++){
+		for (i=0;i<=arrayLength;i++){
+			for(j=0; j<=arrayLength; j++){
 				matrix[i][j] = (int)(Math.random()*10);
 			}
 			System.out.println(Arrays.toString(matrix[i]));
 		}
 		i = 0;
 		System.out.println("Элементы главной диагонали:");
-		for (i=0; i<=index; i++){
+		for (i=0; i<=arrayLength; i++){
 			System.out.print(matrix[i][i]+" ");
 		}
 		System.out.println();
--- Arrays/randomIndexes/randInd.java
+++ Arrays/randomIndexes/randInd.java
@@ -1,23 +1,23 @@
 import java.util.Arrays;
 public class randInd{
 	public static void main(String[] args){
-		int index1, index2,i,j,maxValue;
-		index1 = (int)(Math.random()*10);
-		index2 = (int)(Math.random()*10);
+		int arrayLength, arrayWidth, i, j, maxValue;
+		arrayLength = (int)(Math.random()*10);
+		arrayWidth = (int)(Math.random()*10);
 
-		int[][] matrix = new int [index1+1][index2+1];
+		int[][] matrix = new int [arrayLength+1][arrayWidth+1];
 		
 
-		for (i=0;i<=index1;i++){
-			for(j=0; j<=index2; j++){
+		for (i=0;i<=arrayLength;i++){
+			for(j=0; j<=arrayWidth; j++){
 				matrix[i][j] = (int)(Math.random()*100);
 				System.out.print(matrix[i][j] + " ");
 			}
 			System.out.println(" ");
 		}
 		maxValue = matrix[0][0];
-		for(i=0;i<=index1;i++){
-			for(j=0;j<=index2;j++){
+		for(i=0;i<=arrayLength;i++){
+			for(j=0;j<=arrayWidth;j++){
 				if (maxValue<matrix[i][j]){
 					maxValue = matrix[i][j];
 				}
