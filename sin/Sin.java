01aa0252dabee4d414ab9b0ff449edd5e402bbe1
Schi8m <schi8m@gmail.com>
2021-04-12 00:57:40 +0300

    Sin 2.0

--- sin/Sin.java
+++ sin/Sin.java
@@ -2,31 +2,33 @@ import java.util.Scanner;
 
 public class Sin{
 	public static void main(String[] args){
-		int i = 1;
-		int j = 1;
-		double factorial = 1;
-		double	currentValue = 0;
-		double  previousValue = 0;
-		double sum = 0;
-
 		Scanner scan = new Scanner(System.in);
-		String result = "";
+		double result;
+
 		
 		int arg = 5;
 		
 
 		System.out.println("Введите необходимую точность:");
 		double accuracyValue = scan.nextDouble();	
-		
-		result = getSinValue(accuracyValue, arg, i, j, factorial, currentValue, previousValue, sum);
 
+		result = getSinValue(accuracyValue, arg);
 		System.out.println(result);
 
 	}
 
 
-	public static String getSinValue(double accuracy, int x, int i, int j, double factorial, double currentValue, double previousValue, double sum){
+	public static double getSinValue(double accuracy, int x){
+			int i = 1;
+			int j = 1;
+			double factorial = 1;
+			double	currentValue = 0;
+			double  previousValue = 0;
+			double sum = 0;
+
 		for(double e = 1; e>=accuracy; e = Math.abs(currentValue-previousValue)){
+			
+
 			factorial = getFactorial(j);
 			if (i/2*2 == i){
 				previousValue = sum;
@@ -41,12 +43,18 @@ public class Sin{
 			j += 2;
 			i += 1;
 		}
-		String result = "" + sum;
-		return result;
+		return sum;
 	}
 
-	public static String getSinValue(double accuracy, double x, int i, int j, double factorial, double currentValue, double previousValue, double sum){
+	public static double getSinValue(double accuracy, double x){
+			int i = 1;
+			int j = 1;
+			double factorial = 1;
+			double	currentValue = 0;
+			double  previousValue = 0;
+			double sum = 0;
 		for(double e = 1; e>=accuracy; e = Math.abs(currentValue-previousValue)){
+
 			factorial = getFactorial(j);
 			if (i/2*2 == i){
 				previousValue = sum;
@@ -61,10 +69,10 @@ public class Sin{
 			j += 2;
 			i += 1;
 		}
-		String result = "" + sum;
-		return result;
+		return sum;
 	}
 	public static double getFactorial(int j){
+
 		double factorial = 1;
 		for (int k = 1; k<=j; k++){
 				factorial = factorial * k;
