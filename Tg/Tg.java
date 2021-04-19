8e95197ab238ef45420d4f252a28bde69eb1bae0
Schi8m <schi8m@gmail.com>
2021-04-19 04:24:44 +0300

    Optimized

--- Tg/Tg.java
+++ Tg/Tg.java
@@ -8,42 +8,18 @@ public class Tg{
 		double cos;
 		double tg;
 
-		int arg = 5;
+		int input = 5;
+		double arg = (double)input;
+
 		System.out.println("Введите необходимую точность:");
 		Scanner scan = new Scanner(System.in);
 		accuracyValue = scan.nextDouble();
+
 		sin = getSinValue(accuracyValue, arg);
 		cos = getCosValue(accuracyValue, arg);
 		tg = sin/cos;
+		
 		System.out.println(tg);
-	}
-
-	public static double getSinValue(double accuracy, int x){
-				int i = 1;
-				int j = 1;
-				double factorial = 1;
-				double	currentValue = 0;
-				double  previousValue = 0;
-				double sum = 0;
-
-			for(double e = 1; e>=accuracy; e = Math.abs(currentValue-previousValue)){
-				
-
-				factorial = getFactorial(j);
-				if (i/2*2 == i){
-					previousValue = sum;
-					sum = sum - (double)(Math.pow(x, j))/factorial;
-					currentValue = sum;
-				}else{
-					previousValue = sum;
-					sum = sum + (double)(Math.pow(x, j))/factorial;
-					currentValue = sum;
-				}	
-				factorial = 1;
-				j += 2;
-				i += 1;
-			}
-			return sum;
 	}
 
 	public static double getSinValue(double accuracy, double x){
@@ -55,31 +31,6 @@ public class Tg{
 			double sum = 0;
 		for(double e = 1; e>=accuracy; e = Math.abs(currentValue-previousValue)){
 
-			factorial = getFactorial(j);
-			if (i/2*2 == i){
-				previousValue = sum;
-				sum = sum - (double)(Math.pow(x, j))/factorial;
-				currentValue = sum;
-			}else{
-				previousValue = sum;
-				sum = sum + (double)(Math.pow(x, j))/factorial;
-				currentValue = sum;
-			}	
-			factorial = 1;
-			j += 2;
-			i += 1;
-		}
-		return sum;
-	}
-	public static double getCosValue(double accuracy, int x){
-		int i = 0;
-		int j = 2;
-		double factorial = 1;
-		double	currentValue = 0;
-		double  previousValue = 0;
-		double sum = 1;
-
-		for(double e = 1; e>=accuracy; e = Math.abs(currentValue-previousValue)){
 			factorial = getFactorial(j);
 			if (i/2*2 == i){
 				previousValue = sum;
