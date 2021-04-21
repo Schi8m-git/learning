6b346a111ae9a8eae66f3ccc4d5bdff8d6c14cc9
Schi8m <schi8m@gmail.com>
2021-04-21 23:02:46 +0300

    2.0

--- Classes/Sin.java
+++ Classes/Sin.java
@@ -12,41 +12,33 @@ public class Sin{
 		double sin = getSinValue(accuracy, arg);
 		System.out.println("Sin("+arg+")="+sin);
 	}
-
-	public static double getSinValue(double accuracy, double x){
+public static double getSinValue(double accuracy, double x){
 			int i = 1;
 			int j = 1;
-			double factorial = 1;
-			double	currentValue = 0;
+			double factorial;
+			double	currentValue = 1;
 			double  previousValue = 0;
 			double sum = 0;
+			double e = Math.abs(currentValue-previousValue);
 
-		for(double e = 1; e>=accuracy; e = Math.abs(currentValue-previousValue)){
-
-
+		while(e>=accuracy){
+	
 			factorial = getFactorial(j);
-			if (i/2*2 == i){
-				previousValue = sum;
-				sum = sum - (double)(Math.pow(x, j))/factorial;
-				currentValue = sum;
-			}else{
-				previousValue = sum;
-				sum = sum + (double)(Math.pow(x, j))/factorial;
-				currentValue = sum;
-			}	
-			factorial = 1;
+			previousValue = sum;
+			sum = (i/2*2==i) ? (sum - (double)(Math.pow(x, j))/factorial) : (sum + (double)(Math.pow(x, j))/factorial);
+			currentValue = sum;
+			e = Math.abs(currentValue-previousValue);
 			j += 2;
 			i += 1;
 		}
 		return sum;
 	}
-
 	public static double getFactorial(int j){
+
 		double factorial = 1;
 		for (int k = 1; k<=j; k++){
 				factorial = factorial * k;
 			}
 		return factorial;
 	}
-
 }
