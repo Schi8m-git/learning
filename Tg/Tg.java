746db40fb1127ba324613e07fb1ab54f5c84c640
Schi8m <schi8m@gmail.com>
2021-04-24 23:54:22 +0300

    Tg finaly

--- Tg/Tg.java
+++ Tg/Tg.java
@@ -8,8 +8,7 @@ public class Tg{
 		double cos;
 		double tg;
 
-		int input = 5;
-		double arg = (double)input;
+		int arg = 5;
 
 		System.out.println("Введите необходимую точность:");
 		Scanner scan = new Scanner(System.in);
@@ -22,29 +21,23 @@ public class Tg{
 		System.out.println(tg);
 	}
 
-	public static double getSinValue(double accuracy, double x){
-			int i = 1;
-			int j = 1;
-			double factorial;
-			double	currentValue = 1;
-			double  previousValue = 0;
-			double sum = 0;
-			double e = Math.abs(currentValue-previousValue);
+	public static double getSinValue(double accuracy, int arg){
+		return sinCounter(accuracy, (double) arg);
+	}
 
-		while(e>=accuracy){
-	
-			factorial = getFactorial(j);
-			previousValue = sum;
-			sum = (i/2*2==i) ? (sum - (double)(Math.pow(x, j))/factorial) : (sum + (double)(Math.pow(x, j))/factorial);
-			currentValue = sum;
-			e = Math.abs(currentValue-previousValue);
-			j += 2;
-			i += 1;
-		}
-		return sum;
+	public static double getSinValue(double accuracy, double arg){
+		return sinCounter(accuracy, (double) arg);
+	}
+
+	public static double getCosValue(double accuracy, int x){
+		return сosCounter(accuracy, (double) x);	
 	}
 
 	public static double getCosValue(double accuracy, double x){
+		return сosCounter(accuracy, (double) x);	
+	}
+
+	public static double сosCounter(double accuracy, double x){
 		int i = 0;
 		int j = 2;
 		double factorial;
@@ -57,7 +50,8 @@ public class Tg{
 	
 			factorial = getFactorial(j);
 			previousValue = sum;
-			sum = (i/2*2==i) ? (sum - (double)(Math.pow(x, j))/factorial) : (sum + (double)(Math.pow(x, j))/factorial);
+			double powValue = (double)(Math.pow(x, j))/factorial;
+			sum = (i/2*2==i) ? (sum - powValue) : (sum + powValue);
 			currentValue = sum;
 			e = Math.abs(currentValue-previousValue);
 			j += 2;
@@ -65,6 +59,30 @@ public class Tg{
 		}
 		return sum;
 	}
+
+	public static double sinCounter(double accuracy, double x){
+			int i = 1;
+			int j = 1;
+			double factorial;
+			double	currentValue = 1;
+			double  previousValue = 0;
+			double sum = 0;
+			double e = Math.abs(currentValue-previousValue);
+
+			while(e>=accuracy){
+	
+			factorial = getFactorial(j);
+			previousValue = sum;
+			double powValue = (double)(Math.pow(x, j))/factorial;
+			sum = (i/2*2==i) ? (sum - powValue) : (sum + powValue);
+			currentValue = sum;
+			e = Math.abs(currentValue-previousValue);
+			j += 2;
+			i += 1;
+		}
+		return sum;
+	}
+
 	public static double getFactorial(int j){
 
 		double factorial = 1;
