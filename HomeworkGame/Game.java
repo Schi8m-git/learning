8ada821703c13f83911d42e8f8684ac02f5e47ee
Schi8m <schi8m@gmail.com>
2021-05-25 19:50:54 +0300

    HomeworkGame 2.3

--- HomeworkGame/Game.java
+++ HomeworkGame/Game.java
@@ -4,19 +4,24 @@ import java.lang.Object;
 
 /**
 *@author schi8m
-*@version HomeworkGame in alpha 2.2
-*@since HomeworkGame in alpha 2.1
-* Главный класс программы.
+*@version HomeworkGame in alpha 2.3
+*@since HomeworkGame in alpha 2.2
+* Класс - интерфейс взаимодействия с пользователем.
 */
+
 public class Game{
+
+static int numbersOfUnits[] = new int[4];
+static int numberOfMoves;
+
 	public static void main (String[] args){
 		System.out.println("Привет. Выбери, что тебе"+
 			" нужно:\n1 - Начать игру\n2 - Выход");
 		Scanner scan = new Scanner(System.in);
 		boolean isContinue = true;
 		while (isContinue){
-			int clickCounter = 0;
 			int x = scan.nextInt();
+			
 			switch (x) {
 				case (1): startGame();
 				case (2): isContinue = false;
@@ -32,183 +37,15 @@ public class Game{
 	private static void startGame(){
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Какое количество воинов ты хочешь?");
-		int numberOfWarriors = scan.nextInt();
+		numbersOfUnits[0] = scan.nextInt();
 		System.out.println("Какое количество магов ты хочешь?");
-		int numberOfWizards = scan.nextInt();
+		numbersOfUnits[1] = scan.nextInt();
 		System.out.println("Какое количество птиц ты хочешь?");
-		int numberOfBirds = scan.nextInt();
+		numbersOfUnits[2] = scan.nextInt();
 		System.out.println("Какое количество Икаров ты хочешь?");
-		int numberOfIkars = scan.nextInt();
+		numbersOfUnits[3] = scan.nextInt();
 		System.out.println("Максимальное кол-во ходов:");
-		int numberOfMoves = scan.nextInt();
-		createHeroes(
-			numberOfWarriors, 
-			numberOfWizards, 
-			numberOfBirds, 
-			numberOfIkars,
-			numberOfMoves
-		);
+		numberOfMoves = scan.nextInt();
+		Engine.createHeroes(numbersOfUnits, numberOfMoves);
 	}
-
-	/**
-	* Создаёт юнитов, генерирует им имя и создаёт отдельные 
-	* массивы как длякаждого класса юнитов, так и для всех сразу.
-	*/
-	private static void createHeroes(
-		int numberOfWarriors, 
-		int numberOfWizards,
-		int numberOfBirds,
-		int numberOfIkars,
-		int numberOfMoves
-	){
-		int arrayLength = numberOfWarriors+numberOfWizards+numberOfBirds+numberOfIkars;
-		Unit[] units = new Unit[arrayLength];
-		Unit[] warr = new Unit[numberOfWarriors];
-		Unit[] wizz = new Unit[numberOfWizards];
-		Unit[] birds = new Unit[numberOfBirds];
-		Unit[] ikars = new Unit[numberOfIkars];
-
-		for (int i = 0; i<numberOfWarriors;i++){
-			warr[i] = new Warrior();
-			warr[i].name = "Warrior"+(i+1);
-			units[Unit.count] = warr[i];
-			Unit.count++;
-		}
-		for (int i = 0; i<numberOfWizards;i++){
-			wizz[i] = new Wizard();
-			wizz[i].name = "Wizzard"+(i+1);
-			units[Unit.count] = wizz[i];
-			Unit.count++;
-		}
-		for (int i = 0; i<numberOfBirds;i++){
-			birds[i] = new Bird();
-			birds[i].name = "Bird"+(i+1);
-			units[Unit.count] = birds[i];
-			Unit.count++;
-		}
-		for (int i = 0; i<numberOfIkars;i++){
-			ikars[i] = new Ikar();
-			ikars[i].name = "Ikar"+(i+1);
-			units[Unit.count] = ikars[i];
-			Unit.count++;
-		}
-
-
-		for (int i = 0;i<units.length;i++){
-			units[i].createStats();
-			units[i].attackRange = (((units[i].speed*arrayLength)/100)<1)? 1: (units[i].speed*arrayLength)/100;
-			units[i].damage = (1-units[i].speed/100)*(units[i].attackPower);
-			System.out.println(units[i].toString());
-		}
-		gamesMove(units, numberOfMoves);
-	}
-
-	/**
-	* Реализует ходы.
-	*/
-	public static void gamesMove(Unit[] units, int numberOfMoves){
-		Scanner scan = new Scanner(System.in);
-		for (int i = 1; i<=numberOfMoves;i++){
-			System.out.println("\n Ход №"+i);
-			for (int k = 0; k<units.length;k++){
-
-				
-
-				if (units[k].health>0){
-				int targetNumber = k+1;	
-				for(int z = 0; z<units[k].attackRange;z++){
-					if (targetNumber>=units.length){
-						targetNumber = 0;
-					}
-
-					while (
-						(units[targetNumber].status==Unit.LONG_DEAD)|
-						(targetNumber==k)|
-						(units[targetNumber].status==Unit.DEAD)
-						){
-						targetNumber++;
-						if ((targetNumber)>=units.length){
-						targetNumber = 0;
-						}
-					}
-
-					if ((units[targetNumber] instanceof FlyingUnit)&(units[k] instanceof HickingUnit)){
-						units[targetNumber].health -= (units[k].damage/2);
-						units[targetNumber].checkStatus();
-
-						if(units[targetNumber].status==Unit.ALIVE){
-							System.out.println(units[k].name+
-								" нанес "+(units[k].damage/2)+
-								" урона "+units[targetNumber].name+
-								", теперь у него "+
-							units[targetNumber].health+" хп. Наземный -->> Летающий");
-						} else if(units[targetNumber].status==Unit.DEAD){
-							System.out.println(units[k].name+
-								" нанес "+units[k].damage+
-								" урона "+units[targetNumber].name+
-								" и убил его.");
-						}
-					} else{
-						units[targetNumber].health -= units[k].damage;
-						units[targetNumber].checkStatus();
-						if(units[targetNumber].status==Unit.ALIVE){
-							System.out.println(units[k].name+
-								" нанес "+units[k].damage+
-								" урона "+units[targetNumber].name+
-								" , теперь у него "+
-							units[targetNumber].health+" хп.");
-						} else if(units[targetNumber].status==Unit.DEAD){
-							System.out.println(units[k].name+
-								" нанес "+units[k].damage+
-								" урона "+units[targetNumber].name+
-								" и убил его.");
-						}
-					}
-						
-						targetNumber++;
-						boolean isOnlyOneAlive = scanAlives(units);
-						if (isOnlyOneAlive){
-							break;
-						}
-					}
-				}
-			}
-			System.out.println("\n Итоги хода:\n "+
-				"Внимание! Если в живых остался только один персонаж, лучше закончить игру).");
-			for (int g=0;g<units.length;g++){
-				System.out.println(units[g].toString());
-			}
-			System.out.println("1 - следующий ход\n2 - выход");
-			int z = scan.nextInt();
-			if (z==2){
-				break;
-			}
-		}	
-	}
-
-
-	public static boolean scanAlives(Unit[] units){
-		int deadCounter = 0;
-		int aliveCounter = 0;
-		String winnerName = "";
-		for (int i = 0; i<units.length; i++){
-			if ((units[i].status=="dead")|(units[i].status=="longdead")){
-				deadCounter += 1;
-			} else {
-				aliveCounter += 1;
-			}
-		}
-
-		if (aliveCounter<=1){
-			for (int j = 0; j<units.length; j++){
-				if (units[j].status=="alive"){
-					winnerName = units[j].name;
-				}
-			}
-			System.out.println("Последний выживший "+ winnerName);
-			return true;
-		} else {
-			return false;
-		}
-	}	
 }
